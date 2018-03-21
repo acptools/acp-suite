e83ffadc4ec273680692abec5083fc72a2e3c7e8
Patrik Pekarčík <patrik.pekarcik@upjs.sk>
2018-03-21 23:31:50 +0100

    AST logging is turned off

--- ide/src/main/java/net/acptools/suite/ide/lang/cpp/util/Logger.java
+++ ide/src/main/java/net/acptools/suite/ide/lang/cpp/util/Logger.java
@@ -3,7 +3,7 @@ package net.acptools.suite.ide.lang.cpp.util;
 
 public class Logger {
 
-    private static boolean activate_log = true;
+    private static boolean activate_log = false;
 
     public static void print(String value) {
         if (activate_log)
